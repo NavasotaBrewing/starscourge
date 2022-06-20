a3894572a788dc2d997d3a3cafb4d745c347a656
llamicron <llamicron@gmail.com>
2022-06-20 00:17:03 -0500

    Adds waveui and a font

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
+// Vue Basics
 import { createApp } from 'vue'
 import App from './App.vue'
+// Router
 import router from "./router";
 
-createApp(App).use(router).mount('#app');
+// Fancy stuff, Wave UI
+import WaveUI from 'wave-ui';
+import 'wave-ui/dist/wave-ui.css';
+
+// custom css
+import "./assets/main.css";
+
+console.log(WaveUI);
+
+const app = createApp(App);
+
+// Options go here, if necessary
+new WaveUI(app, {
+    colors: {
+        primary: "#BF7746",
+    }
+});
+
+app.use(router).mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,9 @@
 <template>
-    Home Page
+    <w-button>Testing!</w-button>
+
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore dolore commodi obcaecati, veniam maiores error doloribus alias iure doloremque quia omnis quae numquam. Numquam maxime sit quisquam debitis nam totam?
+
+    <i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur culpa eligendi porro delectus atque, sit illo illum non temporibus tempora molestiae voluptate numquam doloremque reiciendis quod, aut iste! Magnam, a?</i>
 </template>
 
 <script>
