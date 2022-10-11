5023e240b11208ec49bf6d5d7769556a77558288
llamicron <llamicron@gmail.com>
2022-10-11 16:12:28 -0500

    Makes device table much better

--- src/views/Model.vue
+++ src/views/Model.vue
@@ -2,19 +2,18 @@
 
     <w-flex wrap>
         <div class="xs2"></div>
-        <div class="xs10 pa1">
-            <h1>RTUs</h1>
+        <div class="xs12 lg6 pa1">
+            <div class="title1 mb6">All RTUs ({{ RTUs().length }})</div>
 
-            <div class="xs4">
-                <w-card v-for="rtu in RTUs()" :key="rtu.id" :title="rtu.name">
+            <w-card v-for="rtu in RTUs()" :key="rtu.id" :title="rtu.name">
                     <div class="grow">
                         <div class="title3 mb1">Details</div>
                         <hr class="mb2" />
                         <pre><w-list :items="RTUAttributesAsList(rtu)" icon="fa fa-cog"></w-list></pre>
 
-                        <div class="title3 mb1 mt3">Devices</div>
+                        <div class="title3 mb1 mt3">Devices ({{ rtu.devices.length }})</div>
                         <hr class="mb2" />
-                        <w-table class="mb3" v-for="dev in rtu.devices" :key="dev.id" :headers="deviceTableHeaders()" :items="deviceTableItems(dev)" />
+                        <w-table class="mb3" :headers="deviceTableHeaders()" :items="deviceTableItems(rtu.devices)" />
                     </div>
 
                     <template #actions>
@@ -22,7 +21,6 @@
                         <w-button bg-color="success">Test Connection</w-button>
                     </template>
                 </w-card>
-            </div>
         </div>
 
     </w-flex>
@@ -51,29 +49,32 @@ export default {
             list.push(this.makeLabel("Devices Found", rtu.devices.length));
             return list;
         },
+
+        // Gives the headers for the device table in each RTU
         deviceTableHeaders() {
             return [
-                { label: "Attribute",   key: "attr"  },
-                { label: "Value",       key: "value" }
+                { label: "Name", key: "name" },
+                { label: "ID", key: "id" },
+                { label: "Driver", key: "driver" },
+                { label: "Address", key: "addr" },
+                { label: "Controller Address", key: "controller_addr" },
             ];
         },
-        deviceTableItems(device) {
-            return [
-                { attr: "Name", value: device.name },
-                { attr: "ID", value: device.id },
-                { attr: "Driver", value: device.driver },
-                { attr: "Address", value: device.addr },
-                { attr: "Controller Address", value: device.controller_addr },
-            ];
+
+        // Returns all the devices, formatted for the table
+        deviceTableItems(devices) {
+            let items = [];
+            devices.forEach(dev => {
+                items.push({
+                    name: dev.name,
+                    id: dev.id,
+                    driver: dev.driver,
+                    addr: dev.addr,
+                    controller_addr: dev.controller_addr
+                });
+            });
+            return items;
         }
-        // deviceAttibutesAsList(device) {
-        //     let list = [];
-        //     list.push(this.makeLabel("ID", device.id));
-        //     list.push(this.makeLabel("Driver", device.driver));
-        //     list.push(this.makeLabel("Address", device.addr));
-        //     list.push(this.makeLabel("Controller Address", device.controller_addr));
-        //     return list;
-        // }
     }
 }
 
